89b1daf926275317cb2929a1e1baa2168e66006b
kemore trowers <kemore.trowers@mymona.uwi.edu>
2016-11-23 00:16:02 +0000

    final php

--- world.php
+++ world.php
@@ -5,14 +5,27 @@ $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
 
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$country = $_GET['country'];
 
-$stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+if ($country == 'all=true')
+{
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo  $row['name'] . ' is ruled by ' . $row['head_of_state'];
+    }
+    echo '</ul>';
 }
-echo '</ul>';
+else 
+{
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
